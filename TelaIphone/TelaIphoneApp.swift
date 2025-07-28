8441ca4a4e5c46cefc6986e4b9f093947101e7e3
ericdenser <ericdenser7@gmail.com>
2025-07-28 14:21:48 -0300

    Initial Commit

---
+++ TelaIphone/TelaIphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelaIphoneApp.swift
+//  TelaIphone
+//
+//  Created by Eric on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TelaIphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TelaIphoneTests/TelaIphoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  TelaIphoneTests.swift
+//  TelaIphoneTests
+//
+//  Created by Eric on 28/07/25.
+//
+
+import Testing
+@testable import TelaIphone
+
+struct TelaIphoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
